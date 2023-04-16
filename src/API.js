af8923adaf40b3573124f7318200d51b89d70c74
James Murdza <james@jamesmurdza.com>
2023-04-17 00:21:34 +0100

    Fix cache API url.

--- src/API.js
+++ src/API.js
@@ -2,7 +2,7 @@ import { PUBLIC_API_KEY } from "$env/static/public";
 export async function callAPI(URL) {
     // Call the API, wait for response, and parse response.
 
-    let cachedURL = `https://livescore-cache-server.onrender.com/cache?url=${encodeURI(URL)}`
+    let cachedURL = `https://livescore-cache-server.onrender.com/cache?url=${encodeURIComponent(URL)}`
     let response = await fetch(cachedURL, settings);
     let responseData = await response.json();
     if (responseData.message) alert(responseData.message);
